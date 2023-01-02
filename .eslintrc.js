cfd75b71768b5fadf6a6423ba2e33e8f419d5d4c
abayram <38274063+abdullahbayram@users.noreply.github.com>
2023-01-02 22:25:33 +0100

    chore: eslint and prettier configuration IV
    use plugin:prettier/recommended instead prettier at eslintrc for extend part.
    
    add scripts related config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   // make sure put prettier at last(at extends part),
   // so it gets the chance to override other things(https://github.com/prettier/eslint-config-prettier)
-  extends: ['universe', 'prettier'],
+  extends: ['universe', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
