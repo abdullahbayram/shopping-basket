b6aff64315d6b026748accd228b1193bfb2d43a2
abayram <38274063+abdullahbayram@users.noreply.github.com>
2023-01-02 21:57:29 +0100

    chore: eslint(and prettier) configuration part III

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // make sure put prettier at last(at extends part),
+  // so it gets the chance to override other things(https://github.com/prettier/eslint-config-prettier)
   extends: ['universe', 'prettier'],
   plugins: ['prettier'],
   rules: {
