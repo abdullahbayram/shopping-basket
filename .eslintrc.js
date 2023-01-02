9da748bd2877bf7bbcb3b364f850c15841fa176f
abayram <38274063+abdullahbayram@users.noreply.github.com>
2023-01-02 22:51:52 +0100

    chore: eslint configuration V
    -add eslint-config-airbnb and related packages such as eslint-plugin-import,  eslint-plugin-jsx-a11y, eslint-plugin-react, eslint-plugin-react-hooks
    
    -downgrade eslint ˆ8.31.0 to ˆ8.2.0 to comply with eslint-config-airbnb
    
    -fix lint errors coming with airbnb eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
   // make sure put prettier at last(at extends part),
   // so it gets the chance to override other things(https://github.com/prettier/eslint-config-prettier)
-  extends: ['universe', 'plugin:prettier/recommended'],
+  extends: ['universe', 'airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/style-prop-object': 'off',
   },
 };
