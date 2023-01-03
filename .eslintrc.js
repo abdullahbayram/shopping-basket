52b68f8666543ce03567dc52e0b4dabdf2eb05bf
abayram <38274063+abdullahbayram@users.noreply.github.com>
2023-01-03 23:14:19 +0100

    chore: eslint configuration VI
    -install @react-native-community/eslint-config
    -add lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,31 @@
 module.exports = {
   // make sure put prettier at last(at extends part),
   // so it gets the chance to override other things(https://github.com/prettier/eslint-config-prettier)
-  extends: ['universe', 'airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
+  extends: [
+    'universe/native',
+    'airbnb',
+    'airbnb/hooks',
+    '@react-native-community',
+    'plugin:prettier/recommended',
+  ],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
+    'max-lines': ['error', { max: 100, skipComments: true, skipBlankLines: true }],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-console': 'off',
+    // react
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/style-prop-object': 'off',
+    'react/destructuring-assignment': 'warn',
+    'react/prop-types': ['error', { ignore: ['navigation', 'isFocused', 'dispatch'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'react/function-component-definition': [2, { namedComponents: ['arrow-function'] }],
+    // react-hooks
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    // react-native
+    'react-native/no-inline-styles': 'error',
   },
 };
--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import styles from './App.style';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
