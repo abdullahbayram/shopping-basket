ec208e1ae23555326c28eccf599faef488800617
abayram <38274063+abdullahbayram@users.noreply.github.com>
2023-01-09 23:01:05 +0100

    feat: add SafeAreaProvider
    -install react-native-safe-area-context
    -install react-native-elements

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { View } from 'react-native';
 import Text from './src/components/Text/Text';
 import styles from './App.style';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 };
 
