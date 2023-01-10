55b3947a3cac8ddf836dad08a91f380edf303b2c
abayram <38274063+abdullahbayram@users.noreply.github.com>
2023-01-10 21:23:18 +0100

    test(fix): add react-native-safe-area-context to App.test

--- App.test.js
+++ App.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
+import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
 
 import App from './App';
+
+jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
 
 describe('<App />', () => {
   it('renders the description', () => {
